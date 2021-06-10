2a652c1ddf7c8498727c43d7f14a4d7d25a90825
汪耀文 <1328235636@qq.com>
2021-06-10 15:07:43 +0800

    swagger3

--- src/main/java/com/example/demo/controller/QuartzController.java
+++ src/main/java/com/example/demo/controller/QuartzController.java
@@ -1,19 +1,27 @@
 package com.example.demo.controller;
 
 import com.example.demo.quartz.QuartzScheduler;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/quartz")
+@Api
 public class QuartzController {
 
     @Autowired
     private QuartzScheduler quartzScheduler;
 
+    @ApiOperation(value = "开始所有任务")
     @RequestMapping("/startAll")
     public void startAllJob(){
         try {
@@ -23,8 +31,15 @@ public class QuartzController {
         }
     }
 
-    @RequestMapping("/info/{name}/{group}")
-    public String info(@PathVariable(value = "name") String name,@PathVariable(value = "group") String group) throws SchedulerException {
+    @ApiOperation(value = "查看指定任务信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "name" ,value = "名称"),
+            @ApiImplicitParam(name = "group", value = "group")
+    })
+    @GetMapping("/info/{name}/{group}")
+    public String info(
+            @PathVariable(value = "name") String name,
+            @PathVariable(value = "group") String group) throws SchedulerException {
         return quartzScheduler.getJobInfo(name, group);
     }
 
---
+++ src/main/java/com/example/demo/swagger/Swagger2.java
@@ -0,0 +1,44 @@
+package com.example.demo.swagger;
+
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableOpenApi
+public class Swagger2 {
+
+
+    /**
+     * 通过 createRestApi函数来构建一个DocketBean
+     * 函数名,可以随意命名,喜欢什么命名就什么命名
+     */
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                // 页面标题
+                .title("Spring Boot Swagger3")
+                // 条款地址
+                .termsOfServiceUrl("http://localhost:8080/swagger/")
+                .version("1.1")
+                // 描述
+                .description("API 描述")
+                .build();
+    }
+}
